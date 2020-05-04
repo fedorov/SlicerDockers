0df3a1de3314d6b7f52464d92993073b52c161f0
Andrey Fedorov <andrey.fedorov@gmail.com>
2020-05-04 16:30:57 -0400

    update username and use 4.10.2

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 VERSION="latest"
+USER="fedorov"
 
-docker build -t stevepieper/base:$VERSION base
-docker build -t stevepieper/x11:$VERSION x11
-docker build -t stevepieper/slicer-dev:$VERSION slicer-dev
-docker build -t stevepieper/slicer3:$VERSION slicer3
-docker build -t stevepieper/slicer:$VERSION slicer
+docker build -t $USER/base:$VERSION base
+docker build -t $USER/x11:$VERSION x11
+docker build -t $USER/slicer-dev:$VERSION slicer-dev
+docker build -t $USER/slicer3:$VERSION slicer3
+docker build -t $USER/slicer:$VERSION slicer
 
-docker build --no-cache -t stevepieper/slicer-chronicle:$VERSION slicer-chronicle
+docker build --no-cache -t $USER/slicer-chronicle:$VERSION slicer-chronicle
 
-docker build -t stevepieper/slicer-dev:$VERSION slicer-dev
+docker build -t $USER/slicer-dev:$VERSION slicer-dev
